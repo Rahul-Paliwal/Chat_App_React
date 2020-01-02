e25d64250913b16d254423528164c68c09d9ca90
rahul <rahulpaliwal177@gmail.com>
2020-01-02 15:52:24 +0530

    Changes On Frontend

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -4,12 +4,8 @@ import './Input.css';
 
 const Input = ({ setMessage, sendMessage, message }) => (
   <form className="form">
-    <input
-      className="input"
-      type="text"
-      placeholder="Type a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
+    <input className="input" type="text" placeholder="Type a message"
+      value={message}  onChange={({ target: { value } }) => setMessage(value)}
       onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
     />
     <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -3,14 +3,14 @@ import onlineIcon from '../../icons/onlineIcon.png';
 import './TextContainer.css';
 const TextContainer = ({ users }) => (
   <div className="textContainer">
-    <div >
-     <div className="chatinfo_container">
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      </div>
+    <div>
+    <div className="info_container">
+        <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+     </div>
       {
       users
         ? ( 
-          <div>
+          <div className="box_container">
             <div className="chatinfo_container">
             <h1 aria-label="emoji">People currently chatting:❤️</h1>
             </div>
@@ -18,8 +18,8 @@ const TextContainer = ({ users }) => (
               <h2>
                 {users.map(({name}) => (
                   <div key={name} className="activeItem">
-                    {name}
-                    <img alt="Online Icon" src={onlineIcon}/>
+                    <div >{name}</div>
+                    <div ><img alt="Online Icon" src={onlineIcon}/></div>
                   </div>
                 ))}
               </h2>
